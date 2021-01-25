1e228f03724ee78f8434136eb2311c0a3fcb3179
kulomambu <77854129+kulomambu@users.noreply.github.com>
2021-01-26 00:40:29 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$User_Agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36";
+
+$PHPSESSID = "ng5v90msq2c3rtiqmh82tr5gt5";
+
+
+/* To Get The Password Go From This Link
+=> https://exe.io/Passfollowfast */
+
+$password = "EslamTube1827403";
